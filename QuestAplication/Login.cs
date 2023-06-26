14466ce913e77ff068a563e389009916606a608e
dragosdragos935 <dragosdragos935@gmail.com>
2023-06-27 00:19:01 +0300

    first design 1.1

---
+++ QuestAplication/Login.cs
@@ -0,0 +1,35 @@
+namespace QuestAplication
+{
+    public partial class Login : Form
+    {
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+             
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
